c1b2a7ebd0367fa7503085bcc8403ed3327c473f
virco <virco@mobileinno.com>
2013-12-12 17:51:08 +0200

    fix

--- pcompat.c
+++ pcompat.c
@@ -22,12 +22,17 @@ char *psync_get_default_database_path(){
   struct passwd pwd;
   struct passwd *result;
   struct stat st;
+  const char *dir;
   char buff[4096];
-  if (getpwuid_r(getuid(), &pwd, buff, sizeof(buff), &result)!=0)
+  dir=getenv("HOME");
+  if (!dir || stat(dir, &st) || !S_ISDIR(st.st_mode)){
+    if (getpwuid_r(getuid(), &pwd, buff, sizeof(buff), &result)!=0)
+      return NULL;
+    dir=result->pw_dir;
+  }
+  if (stat(dir, &st) || !S_ISDIR(st.st_mode))
     return NULL;
-  if (stat(result->pw_dir, &st) || !S_ISDIR(st.st_mode))
-    return NULL;
-  return psync_strcat(result->pw_dir, "/", DEFAULT_POSIX_DBNAME, NULL);
+  return psync_strcat(dir, "/", DEFAULT_POSIX_DBNAME, NULL);
 #elif defined(WINDOWS)
 #error "Need Windows implementation"
 #else
