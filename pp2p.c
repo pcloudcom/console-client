485f41d0c0a0ea24998299e203e090ed0d492e6d
peshe <peter.armianov@mobileinno.com>
2014-02-10 12:21:49 +0200

    Windows fixes

--- pp2p.c
+++ pp2p.c
@@ -1,7 +1,7 @@
 /* Copyright (c) 2014 Anton Titov.
  * Copyright (c) 2014 pCloud Ltd.
  * All rights reserved.
- * 
+ *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are met:
  *     * Redistributions of source code must retain the above copyright
@@ -12,7 +12,7 @@
  *     * Neither the name of pCloud Ltd nor the
  *       names of its contributors may be used to endorse or promote products
  *       derived from this software without specific prior written permission.
- * 
+ *
  * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
  * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
@@ -331,7 +331,7 @@ int psync_p2p_check_download(psync_fileid_t fileid, const unsigned char *filehas
     psync_milisleep(PSYNC_P2P_SLEEP_WAIT_DOWNLOAD);
     goto err_temp;
   }
-  
+
 err_perm:
   for (i=0; i<il->interfacecnt; i++)
     if (sockets[i]!=INVALID_SOCKET)
