7fd29a88accd3ac7d6ef04152b19481e2f3cc3b1
virco <virco@mobileinno.com>
2014-03-20 14:57:39 +0200

    reject new syncs that are subfolders or parents of current sync

--- psynclib.h
+++ psynclib.h
@@ -168,6 +168,7 @@ typedef struct {
 #define PERROR_INVALID_SYNCTYPE        10
 #define PERROR_OFFLINE                 11
 #define PERROR_INVALID_SYNCID          12
+#define PERROR_PARENT_OR_SUBFOLDER_ALREADY_SYNCING 13
 
 #define PLIST_FILES   1
 #define PLIST_FOLDERS 2
