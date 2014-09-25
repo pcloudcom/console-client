0934e7cf2b06f4509494b170a1b58c3790339b1f
virco <virco@mobileinno.com>
2014-09-25 14:51:49 +0300

    fix login and fs mount on mac

--- psettings.h
+++ psettings.h
@@ -32,7 +32,7 @@
 #include "pcompiler.h"
 #include "pcompat.h"
 
-#define PSYNC_LIB_VERSION "1.1.0"
+#define PSYNC_LIB_VERSION "1.3.0"
 
 #define PSYNC_API_HOST     "binapi.pcloud.com"
 #define PSYNC_API_PORT     80
