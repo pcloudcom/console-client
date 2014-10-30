31176d66c66d463c9e0c55b04b7d0da57437b48b
virco <virco@mobileinno.com>
2014-10-30 16:13:34 +0200

    fix

--- pfs.c
+++ pfs.c
@@ -1865,6 +1865,8 @@ static int psync_fs_statfs(const char *path, struct statvfs *stbuf){
   memset(stbuf, 0, sizeof(struct statvfs));
   q=psync_get_uint_value("quota");
   uq=psync_get_uint_value("usedquota");
+  if (uq>q)
+    uq=q;
   stbuf->f_bsize=FS_BLOCK_SIZE;
   stbuf->f_frsize=FS_BLOCK_SIZE;
   stbuf->f_blocks=q/FS_BLOCK_SIZE;
