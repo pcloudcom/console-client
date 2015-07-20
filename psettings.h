061a5042ee5ac583c01c5061b769b95c35168948
Ivan Stoev <ivan.stoev@pcloud.com>
2015-07-20 14:55:25 +0300

    Back to api.pcloud.com.

--- psettings.h
+++ psettings.h
@@ -40,21 +40,21 @@
 #define PSYNC_API_PORT_SSL 8399
 */
 
-// #define PSYNC_API_HOST     "binapi.pcloud.com"
-// #define PSYNC_API_PORT     80
-// #define PSYNC_API_PORT_SSL 443
-// 
-// #define PSYNC_API_AHOST     "api.pcloud.com"
-// #define PSYNC_API_APORT     8398
-// #define PSYNC_API_APORT_SSL 8399
-
-#define PSYNC_API_HOST     "binapi69.pcloud.com"
+#define PSYNC_API_HOST     "binapi.pcloud.com"
 #define PSYNC_API_PORT     80
 #define PSYNC_API_PORT_SSL 443
 
-#define PSYNC_API_AHOST     "api69.pcloud.com"
+#define PSYNC_API_AHOST     "api.pcloud.com"
 #define PSYNC_API_APORT     8398
 #define PSYNC_API_APORT_SSL 8399
+
+// #define PSYNC_API_HOST     "binapi69.pcloud.com"
+// #define PSYNC_API_PORT     80
+// #define PSYNC_API_PORT_SSL 443
+// 
+// #define PSYNC_API_AHOST     "api69.pcloud.com"
+// #define PSYNC_API_APORT     8398
+// #define PSYNC_API_APORT_SSL 8399
 
 #define PSYNC_P2P_PORT 42420
 
