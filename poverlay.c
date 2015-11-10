84fa371b4acdd9af08d62d5da8e057976be5580c
Ivan Stoev <ivan.stoev@pcloud.com>
2015-11-10 14:42:53 +0200

    New overlay for linux and macosx

--- poverlay.c
+++ poverlay.c
@@ -25,143 +25,23 @@
 */
 #include "pcompat.h"
 #include "plibs.h"
+#include "poverlay.h"
+
 
 #if defined(P_OS_WINDOWS)
 
-#include <windows.h>
-#include <stdio.h>
-#include <tchar.h>
-#include <strsafe.h>
+#include "poverlay_win.c"
 
-#define POVERLAY_BUFSIZE 512
+#elif defined(P_OS_LINUX) || definef(P_OS_MACOSX) || defined(P_OS_BSD)
 
+#include "poverlay_lin.c"
 
-#include "poverlay.h"
+#else 
 
-LPCWSTR PORT = TEXT("\\\\.\\pipe\\pStatusPipe");
+void overlay_main_loop(VOID){}
+void instance_thread(LPVOID){}
 
-void overlay_main_loop(VOID)
-{
-  BOOL   fConnected = FALSE;
-  HANDLE hPipe = INVALID_HANDLE_VALUE;
-
-  // The main loop creates an instance of the named pipe and
-  // then waits for a client to connect to it. When the client
-  // connects, a thread is created to handle communications
-  // with that client, and this loop is free to wait for the
-  // next client connect request. It is an infinite loop.
-
-  for (;;)
-  {
-    debug(D_NOTICE, "\nPipe Server: Main thread awaiting client connection on %s\n", PORT);
-    hPipe = CreateNamedPipe(
-      PORT,                     // pipe name
-      PIPE_ACCESS_DUPLEX,       // read/write access
-      PIPE_TYPE_MESSAGE |       // message type pipe
-      PIPE_READMODE_MESSAGE |   // message-read mode
-      PIPE_WAIT,                // blocking mode
-      PIPE_UNLIMITED_INSTANCES, // max. instances
-      POVERLAY_BUFSIZE,         // output buffer size
-      POVERLAY_BUFSIZE,         // input buffer size
-      0,                        // client time-out
-      NULL);                    // default security attribute
-
-    if (hPipe == INVALID_HANDLE_VALUE)
-    {
-      debug(D_NOTICE, "CreateNamedPipe failed, GLE=%d.\n", GetLastError());
-      return;
-    }
-
-    fConnected = ConnectNamedPipe(hPipe, NULL) ?
-    TRUE : (GetLastError() == ERROR_PIPE_CONNECTED);
-
-    if (fConnected)
-    {
-      debug(D_NOTICE, "Client connected, creating a processing thread.\n");
-
-      // Create a thread for this client.
-      psync_run_thread1(
-        "Pipe request handle routine",
-        instance_thread,    // thread proc
-        (LPVOID)hPipe     // thread parameter
-        );
-    }
-    else
-      CloseHandle(hPipe);
-  }
-
-  return;
-}
-
-void instance_thread(LPVOID lpvParam)
-{
-  DWORD cbBytesRead = 0, cbWritten = 0;
-  BOOL fSuccess = FALSE;
-  HANDLE hPipe = NULL;
-  char  chBuf[POVERLAY_BUFSIZE];
-
-  message* request = NULL; //(message*)psync_malloc(POVERLAY_BUFSIZE);
-  message* reply = (message*)psync_malloc(POVERLAY_BUFSIZE);
- // memset(request,0,sizeof(request));
-  memset(reply, 0, sizeof(reply));
-  if (lpvParam == NULL)
-  {
-    debug(D_ERROR, "InstanceThread got an unexpected NULL value in lpvParam.\n");
-    return;
-  }
-
-  // debug(D_NOTICE, "InstanceThread created, receiving and processing messages.\n");
-  hPipe = (HANDLE)lpvParam;
-  while (1)
-  {
-    do
-    {
-      fSuccess = ReadFile(
-        hPipe,    // pipe handle 
-        chBuf,    // buffer to receive reply 
-        POVERLAY_BUFSIZE,  // size of buffer 
-        &cbBytesRead,  // number of bytes read 
-        NULL);    // not overlapped 
-
-      if (!fSuccess && GetLastError() != ERROR_MORE_DATA)
-        break;
-    } while (!fSuccess);  // repeat loop if ERROR_MORE_DATA 
-
-    if (!fSuccess || cbBytesRead == 0)
-    {
-      if (GetLastError() == ERROR_BROKEN_PIPE)
-        debug(D_NOTICE, "InstanceThread: client disconnected.\n");
-      else
-
-        debug(D_NOTICE, "InstanceThread ReadFile failed, GLE=%d.\n", GetLastError());
-      break;
-    }
-    message *request = (message *)chBuf;
-
-    debug(D_NOTICE, "bytes received  %d buffer[%s]\n", cbBytesRead, chBuf);
-    get_answer_to_request(request, reply);
-    fSuccess = WriteFile(
-      hPipe,        // handle to pipe
-      reply,     // buffer to write from
-      reply->length, // number of bytes to write
-      &cbWritten,   // number of bytes written
-      NULL);        // not overlapped I/O
-
-    if (!fSuccess || reply->length != cbWritten)
-    {
-      debug(D_NOTICE, "InstanceThread WriteFile failed, GLE=%d.\n", GetLastError());
-      break;
-    }
-  }
-  FlushFileBuffers(hPipe);
-  DisconnectNamedPipe(hPipe);
-  CloseHandle(hPipe);
-  psync_free(request);
-  psync_free(reply);
-
-  debug(D_NOTICE, "InstanceThread exitting.\n");
-  return;
-}
+#endif //defined(P_OS_WINDOWS)
 
 void get_answer_to_request(message *request, message *replay)
 {
@@ -187,4 +67,4 @@ void get_answer_to_request(message *request, message *replay)
   strncpy(replay->value, msg, 4);
 
 }
-#endif //defined(P_OS_WINDOWS)
+
