ea8f95f11cfca71f71dab32c251e6b05ec058d20
Ivan Stoev <ivan.stoev@pcloud.com>
2016-08-24 12:28:12 +0300

    Crypto time trace.

--- pdiff.c
+++ pdiff.c
@@ -1260,6 +1260,7 @@ static void process_modifyuserinfo(const binresult *entry){
   psync_sql_bind_string(q, 1, "cryptoexpires");
   psync_sql_bind_uint(q, 2, crexp);
   psync_sql_run(q);
+  debug(D_NOTICE, "Tracing crypto time - cryptoexpires [%lld] psync_millitime [%lld]",(long long)crexp, (long long)psync_millitime());
   if (psync_is_business || crsub){
     if (crst)
       crstat = 5;
