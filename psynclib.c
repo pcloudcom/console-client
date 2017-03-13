23ac7dba0e0a82b2f398e7cf6151b6671b5eb715
Ivan Stoev <ivan.stoev@pcloud.com>
2017-03-13 10:22:59 +0200

    Adding 30 days expiration of screenshot links.

--- psynclib.c
+++ psynclib.c
@@ -1767,7 +1767,7 @@ int64_t psync_file_public_link(const char *path, char **code /*OUT*/, char **err
 }
 
 int64_t psync_screenshot_public_link(const char *path, char **code /*OUT*/, char **err /*OUT*/) {
-  int64_t res = do_psync_file_public_link(path, code, err,  psync_timer_time() + 604800 , 0, 0);
+  int64_t res = do_psync_file_public_link(path, code, err,  psync_timer_time() + 2592000 , 0, 0);
   if (res == 2261) {
     psync_free(*err);
     res =  do_psync_file_public_link(path, code, err, 0, 0, 0);
