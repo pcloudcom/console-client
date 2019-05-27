623565f379db9c480532e389021ae75b1ed2e75d
Gordin <9ordin@gmail.com>
2019-05-27 19:09:43 +0200

    Fixes big cache and README

--- install_debian.sh
+++ install_debian.sh
@@ -1,17 +1,17 @@
 #!/bin/sh
-sudo apt-get install cmake zlib1g-dev libboost-system-dev libboost-program-options-dev libpthread-stubs0-dev libfuse-dev libudev-dev git 
-mkdir console-client   
-git clone https://github.com/pcloudcom/console-client.git ./console-client/  
-cd ./console-client/pCloudCC/   
-cd lib/pclsync/        
-make clean    
-make fs     
-cd ../mbedtls/   
-cmake .      
-make clean     
-make       
-cd ../..      
-cmake .    
-make      
-sudo make install     
-ldconfig     
+sudo apt-get install cmake zlib1g-dev libboost-system-dev libboost-program-options-dev libpthread-stubs0-dev libfuse-dev libudev-dev git
+mkdir console-client
+git clone https://github.com/pcloudcom/console-client.git ./console-client/
+cd ./console-client/pCloudCC/
+cd lib/pclsync/
+make clean
+make fs
+cd ../mbedtls/
+cmake .
+make clean
+make
+cd ../..
+cmake .
+make
+sudo make install
+ldconfig
